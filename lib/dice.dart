f6b195ebbf1930fb0d339b8aa9fcaeee8ef20a95
bmmaker <bmmaker35@gmail.com>
2023-02-04 10:30:51 +0900

    주사위 게임 완성

--- lib/dice.dart
+++ lib/dice.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Dice extends StatefulWidget {
   const Dice({super.key});
@@ -8,6 +10,8 @@ class Dice extends StatefulWidget {
 }
 
 class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 1;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,13 +19,74 @@ class _DiceState extends State<Dice> {
         theme: ThemeData(primarySwatch: Colors.red),
         title: 'dice app',
         home: Scaffold(
+          backgroundColor: Colors.redAccent,
           appBar: AppBar(
+            backgroundColor: Colors.redAccent,
             title: const Text('Dice'),
             centerTitle: true,
           ),
-          body: const Center(
-            child: Text('dice game page'),
-          ),
+          body: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        'image/dice$leftDice.png',
+                        width: 300,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20.0,
+                    ),
+                    Expanded(
+                      child: Image.asset(
+                        'image/dice$rightDice.png',
+                        width: 300,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 60.0,
+              ),
+              ButtonTheme(
+                minWidth: 100.0,
+                height: 60.0,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.orangeAccent),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      leftDice = Random().nextInt(6) + 1;
+                      rightDice = Random().nextInt(6) + 1;
+                    });
+                    showToast(
+                        'Left dice: {$leftDice}, Right dice: {$rightDice}');
+                  },
+                  child: const Icon(
+                    Icons.play_arrow,
+                    color: Colors.white,
+                    size: 50.0,
+                  ),
+                ),
+              )
+            ],
+          )),
         ));
   }
+}
+
+void showToast(String message) {
+  Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -42,78 +42,86 @@ class _LogInState extends State<LogIn> {
         ),
         actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
       ),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          const Padding(padding: EdgeInsets.only(top: 50)),
-          const Center(
-            child: Image(
-              image: AssetImage('image/chef.gif'),
-              width: 170.0,
-              height: 190.0,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Column(children: [
+            const Padding(padding: EdgeInsets.only(top: 50)),
+            const Center(
+              child: Image(
+                image: AssetImage('image/chef.gif'),
+                width: 170.0,
+                height: 190.0,
+              ),
             ),
-          ),
-          Form(
-              child: Theme(
-            data: ThemeData(
-                primaryColor: Colors.teal,
-                inputDecorationTheme: const InputDecorationTheme(
-                    labelStyle: TextStyle(color: Colors.teal, fontSize: 15.0))),
-            child: Container(
-              padding: const EdgeInsets.all(40.0),
-              child: Column(children: [
-                TextField(
-                  controller: controller1,
-                  decoration: const InputDecoration(labelText: 'Enter "dice"'),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                TextField(
-                  controller: controller2,
-                  decoration:
-                      const InputDecoration(labelText: 'Enter password'),
-                  keyboardType: TextInputType.text,
-                  obscureText: true,
-                ),
-                const SizedBox(
-                  height: 40.0,
-                ),
-                ButtonTheme(
-                    minWidth: 100.0,
-                    height: 50.0,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.orangeAccent),
-                      ),
-                      onPressed: () {
-                        if (controller1.text == 'dice' &&
-                            controller2.text == '1234') {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (BuildContext context) =>
-                                      const Dice()));
-                        } else {
-                          ScaffoldMessenger.of(context)
-                              .showSnackBar(const SnackBar(
-                            content: Text(
-                              '로그인 정보를 확인하세요',
-                              textAlign: TextAlign.center,
-                            ),
-                            duration: Duration(seconds: 2),
-                            backgroundColor: Colors.blue,
-                          ));
-                        }
-                      },
-                      child: const Icon(
-                        Icons.arrow_forward,
-                        color: Colors.white,
-                        size: 35.0,
-                      ),
-                    ))
-              ]),
-            ),
-          ))
-        ]),
+            Form(
+                child: Theme(
+              data: ThemeData(
+                  primaryColor: Colors.teal,
+                  inputDecorationTheme: const InputDecorationTheme(
+                      labelStyle:
+                          TextStyle(color: Colors.teal, fontSize: 15.0))),
+              child: Container(
+                padding: const EdgeInsets.all(40.0),
+                child: Column(children: [
+                  TextField(
+                    autofocus: true,
+                    controller: controller1,
+                    decoration:
+                        const InputDecoration(labelText: 'Enter "dice"'),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  TextField(
+                    controller: controller2,
+                    decoration:
+                        const InputDecoration(labelText: 'Enter password'),
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 40.0,
+                  ),
+                  ButtonTheme(
+                      minWidth: 100.0,
+                      height: 50.0,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.orangeAccent),
+                        ),
+                        onPressed: () {
+                          if (controller1.text == 'dice' &&
+                              controller2.text == '1234') {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        const Dice()));
+                          } else {
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(const SnackBar(
+                              content: Text(
+                                '로그인 정보를 확인하세요',
+                                textAlign: TextAlign.center,
+                              ),
+                              duration: Duration(seconds: 2),
+                              backgroundColor: Colors.blue,
+                            ));
+                          }
+                        },
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                          size: 35.0,
+                        ),
+                      ))
+                ]),
+              ),
+            ))
+          ]),
+        ),
       ),
     );
   }
